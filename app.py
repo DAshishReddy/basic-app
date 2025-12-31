f496c5b84eaa572c364d196692188be890bcd090
Ashish <54053045+DAshishReddy@users.noreply.github.com>
2025-12-31 12:37:48 +0530

    Update greeting message in Flask app

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello():
-    return "Hello from Python App via Jenkins + ArgoCD + Kubernetes!"
+    return "Hello from Ash to Python App via Jenkins + ArgoCD + Kubernetes!"
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
